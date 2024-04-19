90be28c7f5f8d114296c0bf3d09352c4910d5edb
saharsh-07 <109348366+saharsh-07@users.noreply.github.com>
2024-04-19 18:47:54 +0530

    code upto graphics - scrolling text

--- Browser.py
+++ Browser.py
@@ -1,26 +1,48 @@
 import tkinter
 from URL import *
 
+# page coordination helper function
+def layout(text):
+  display_list = [] # display_list is standard name for list of things to display
+  cursor_x, cursor_y = HSTEP, VSTEP 
+  for c in text:
+    display_list.append((cursor_x, cursor_y, c)) # horizontal, vertical delta, characters in text
+    cursor_x += HSTEP # keep displaying with diff of horiz delta
+    if cursor_x >= WIDTH - HSTEP: # if end of line (line-wrap)
+      cursor_y += VSTEP # next column (verical)
+      cursor_x = HSTEP  # start of row (horizontal)
+
+  return display_list
+
+
 class Browser:
   def __init__(self):
-    global HEIGHT
-    global WIDTH
-    HEIGHT, WIDTH = 600, 800
+    global HEIGHT, WIDTH, HSTEP, VSTEP, SCROLL_STEP
+    HEIGHT, WIDTH = 600, 800  # height and width of window
+    HSTEP, VSTEP = 13, 18 # vertical and horizontal placing delta
+    self.scroll = 0 # how much scrolled
+    SCROLL_STEP = 100  # scroll this much scrolled using button
     self.window = tkinter.Tk()
     self.canvas = tkinter.Canvas(self.window, height=HEIGHT, width=WIDTH)
-    self.canvas.pack()
+    self.canvas.pack()  # keeps canvas under window
+    self.window.bind("<Down>", self.scroll_down)  # event of down keypress (for scrolling)
   
+  # helper function for scrolling
+  def scroll_down(self, e):
+    self.scroll += SCROLL_STEP
+    self.draw()
+
+  # function for displaying
+  def draw(self):
+    self.canvas.delete("all") # repainting every time
+    for x, y, c in self.display_list:
+      self.canvas.create_text(x, y - self.scroll, text=c)
+
   def load(self, url):
-    HSTEP, VSTEP = 13, 18 # vertical and horizontal placing delta
-    cursor_x, cursor_y = HSTEP, VSTEP
     body = URL.request(url)
     text = lex(body)
-    for c in text:
-      self.canvas.create_text(cursor_x, cursor_y, text=c)
-      cursor_x += HSTEP
-      if cursor_x >= WIDTH - HSTEP:
-        cursor_y += VSTEP
-        cursor_x = HSTEP
+    self.display_list = layout(text)
+    self.draw()
         
 
 if __name__ == "__main__":
--- URL.py
+++ URL.py
@@ -3,20 +3,29 @@ import ssl
 
 class URL:
   def __init__(self, url):
+    """
+    All below code does is URI resolution.
+    parts URI into protocol(scheme), host, port, path, etc
+    """
     self.scheme, url = url.split("://", 1)
     assert self.scheme in ["http", "https"]
+    
     if "/" not in url:
       url = url + "/"   # for handling path
+    
     self.host, url = url.split("/", 1)
     self.path = "/" + url
+    
     if self.scheme == "http":
       self.port = 80
     elif self.scheme == "https":
       self.port = 443
+    
     if ":" in url:
       self.host, self.port = self.host.split(":", 1)
       self.port = int(port)
-    
+
+  # get web content from internet 
   def request(self):
     # AF - address family(various communication ways eg. internet , bluetooth, etc)
     # type - type of communication protocol (like stream, datagram , raw, etc)
@@ -25,30 +34,39 @@ class URL:
                type = socket.SOCK_STREAM,
                proto = socket.IPPROTO_TCP)
     s.connect((self.host, self.port))
-    if self.scheme == "https":
-      ctx = ssl.create_default_context()
+    
+    if self.scheme == "https":  # creation of https connection is diff than http
+      ctx = ssl.create_default_context()  # secure socket
       s = ctx.wrap_socket(s, server_hostname=self.host)
+
     req = "GET {} HTTP/1.0\r\n".format(self.path)
     req += "Host: {}\r\n".format(self.host)
     req += "\r\n"
+    
     s.send(req.encode("utf-8"))
     res = s.makefile("r", encoding="utf-8", newline="\r\n")
     status_line = res.readline()
     version, status, explanation = status_line.split(" ", 2)
+    
     res_headers = {}
     while True:
       line = res.readline()
+    
       if line == "\r\n":
         break
+    
       header, value = line.split(":", 1)
       res_headers[header.casefold()] = value.strip()
+    
     assert "transfer-encoding" not in res_headers
     assert "content-encoding" not in res_headers
+    
     content = res.read()
     s.close()
 
     return content
-  
+
+# text processing 
 def lex(body):
   text = ""
   in_tag = False
@@ -61,9 +79,11 @@ def lex(body):
       text += c
   return text
 
+# loading of content from web 
 def load(url):
   body = url.request()
   lex(body)
+  
   
 if __name__ == "__main__":
   import sys
