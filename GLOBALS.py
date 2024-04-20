f1e5dc63f18caf4287f2d443aa4bd7af35b34632
saharsh-07 <109348366+saharsh-07@users.noreply.github.com>
2024-04-20 17:49:58 +0530

    moved all global variable to seperate file

---
+++ GLOBALS.py
@@ -0,0 +1,6 @@
+global HEIGHT, WIDTH, HSTEP, VSTEP, SCROLL_STEP
+global BI_TIMES
+
+HEIGHT, WIDTH = 600, 800  # height and width of window
+SCROLL_STEP = 100  # scroll this much scrolled using button
+HSTEP, VSTEP = 13, 18 # vertical and horizontal placing delta
